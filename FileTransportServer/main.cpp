c53545d19084b389d3768bba41799dbf0b77c219
Arthur Wang <arthurwzh@sina.cn>
2020-09-19 00:48:55 -0400

    Initial Commit

---
+++ FileTransportServer/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  FileTransportServer
+//
+//  Created by 王子豪 on 9/19/20.
+//  Copyright © 2020 王子豪. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
